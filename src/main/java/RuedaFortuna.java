ca161b6f75ac6bdc734b31edf69ca6e22adcb1e7
Bayron <b.munoz15@ufromail.com>
2020-06-13 16:01:34 -0500

    agregar limites al numero aleatorio

--- src/main/java/RuedaFortuna.java
+++ src/main/java/RuedaFortuna.java
@@ -12,7 +12,7 @@ public class RuedaFortuna {
         casillas = new int[numeroCasillas];
         //ciclo para inicializar la ruleta
         for (int i = 0; i < casillas.length; i++) {
-            casillas[i] = rnum.nextInt();
+            casillas[i] = rnum.nextInt(100)*100;
             //para que no se repita se hara otro arreglo de largo de verificacion, para que no se repitan el numero
             for (int j = 0; j < i; j++) {
                 //si casillas i = casillas j se reducira i y buscara otro numero hasta que sean distintos y el programa siga con su trayecto
@@ -20,6 +20,10 @@ public class RuedaFortuna {
                     i--;
                 }
             }
+        }
+        for (int i = 0; i <casillas.length; i++) {
+            System.out.println(casillas[i]);
+
         }
     }
 
