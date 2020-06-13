703bb0eed11edd8b2d9fce7e7523aba29e0489a1
Bayron <b.munoz15@ufromail.com>
2020-06-13 15:04:16 -0500

    Agregar metodo que pregunta el numero de casillas y crear clase test

--- src/main/java/RuedaFortuna.java
+++ src/main/java/RuedaFortuna.java
@@ -1,10 +1,21 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class RuedaFortuna {
     private int[] casillas;
+    Random rnum = new Random();
+    Scanner in  = new Scanner(System.in);
+
     public void inicializarRueda(int numeroCasillas){
-        //..
+        numeroCasillas = preguntaNumeroCasillas();
     }
     public int lanzarRueda(){
         //..
         return 0;
+    }
+    public int preguntaNumeroCasillas(){
+        System.out.println("ingrese el número de casillas de la ruleta");
+        int a = in.nextInt();
+        return a;
     }
 }
---
+++ src/test/java/RuedaFortunaTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+public class RuedaFortunaTest {
+
+    @org.junit.Test
+    public void inicializarRueda() {
+    }
+
+    @org.junit.Test
+    public void lanzarRueda() {
+    }
+
+    @org.junit.Test
+    public void preguntaNumeroCasillas() {
+    }
+}
