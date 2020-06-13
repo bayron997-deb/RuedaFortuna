5b90cb3cbdb61654f305e228548abad939ad495e
Bayron <b.munoz15@ufromail.com>
2020-06-13 14:46:24 -0500

    Agregar estructura pedida en tarea

--- src/main/java/RuedaFortuna.java
+++ src/main/java/RuedaFortuna.java
@@ -1,2 +1,10 @@
 public class RuedaFortuna {
+    private int[] casillas;
+    public void inicializarRueda(int numeroCasillas){
+        //..
+    }
+    public int lanzarRueda(){
+        //..
+        return 0;
+    }
 }
